700d2cbdcdf060e4c044b05193180b9a16ac022e
Slam310 <francois.greze@devmeup.net>
2016-11-22 00:11:41 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
-  name: 'mpaddock:browser-notifications',
-  version: '0.1.0',
-  summary: 'Send browser notifications to clients from a Meteor server.',
-  git: 'https://github.com/mpaddock/meteor-browser-notifications',
+  name: 'wooterz:browser-notifications',
+  version: '0.0.1',
+  summary: 'Browser Notifications Api with Meteor.',
+  git: 'https://github.com/Wooterz/meteor-browser-notifications',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('coffeescript');
   api.use('mongo');
   api.export('BrowserNotifications');
-  api.addFiles('browser-notifications.coffee');
+  api.addFiles('browser-notifications.js');
 });
