9713efe67aac39ae9bb2c31be7c9892ab906d791
Slam310 <francois.greze@devmeup.net>
2016-11-22 09:26:08 +0100

    Update and rename browser-notifications.js to browsifications.js

--- browsifications.js
+++ browsifications.js
@@ -1,25 +1,25 @@
-const BrowserNotifications;
+const Browsifications;
 
-BrowserNotifications = new Mongo.Collection('browserNotifications');
+Browsifications = new Mongo.Collection('browsifications');
 
 if (Meteor.isServer) {
-  BrowserNotifications.sendNotification = function(opts) {
+  Browsifications.sendNotification = function(opts) {
     var id;
-    id = BrowserNotifications.insert({
+    id = Browsifications.insert({
       userId: opts.userId,
       title: opts.title,
       body: opts.body
     });
     return Meteor.setTimeout(function() {
-      return BrowserNotifications.remove(id);
+      return Browsifications.remove(id);
     }, 60000);
   };
   Meteor.publish('notifications', function() {
-    return BrowserNotifications.find({
+    return Browsifications.find({
       userId: this.userId
     });
   });
-  BrowserNotifications.allow({
+  Browsifications.allow({
     insert: function() {
       return false;
     },
@@ -34,14 +34,14 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
   Meteor.subscribe('notifications');
-  BrowserNotifications.find().observe({
+  Browsifications.find().observe({
     added: function(doc) {
       Notification.requestPermission(function(p) {
         return new Notification(doc.title, {
           body: doc.body
         });
       });
-      return BrowserNotifications.remove(doc._id);
+      return Browsifications.remove(doc._id);
     }
   });
 }
