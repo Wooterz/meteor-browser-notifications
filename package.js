776e7a95e18dec0fb9229a8739cd98f2d9ebc6ff
Slam310 <francois.greze@devmeup.net>
2016-11-22 09:26:40 +0100

    Update package.js

--- package.js
+++ package.js
@@ -9,6 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('mongo');
-  api.export('BrowserNotifications');
+  api.export('Browsifications');
   api.addFiles('browsifications.js');
 });
